c4ebc20560cb17121bb39cc54e2e31df4be09f67
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2022-11-16 00:36:11 +0100

    nodemon, express, update readme

---
+++ README.md
@@ -0,0 +1,6 @@
+### Running the Development Server
+
+- clone the repository to your local machine with `git clone (url)``
+- Open the cloned folder in your favorite IDE, preferrably VScode.
+- From your IDE terminal run `npm install`to download all of the developmental dependencies
+- Run `npm run dev`to launch the development server on the designated port 3001 on localhost
--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const hello = () => {};
+const { response } = require('express');
+const express = require('express');
 
-hello();
+const app = express();
+
+// define temporary data jason here(to be replaced by DB)
+let data = [
+  {
+    id: 1,
+    name: 'samuel nzekwe',
+    address: 'oranienstrs 204 Berlin',
+  },
+];
+
+//get all data
+app.get('/api/users', (request, response) => {
+  response.json(data);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
