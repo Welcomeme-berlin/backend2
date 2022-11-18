c9206447ffcaadd2e4dbd2b600a7d9681c975809
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2022-11-18 16:03:06 +0100

    adding users to database

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,17 @@
 const bcrypt = require('bcrypt'); //to hash user  password
 const usersRouter = require('express').Router();
-const User = require('../models/user');
+const User = require('../models/users');
 
 usersRouter.post('/', async (request, response) => {
   const { username, firstname, lastname, location, phone, email, password } =
     request.body;
+
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    return response.status(400).json({
+      error: 'username must be unique',
+    });
+  }
 
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
@@ -22,6 +29,12 @@ usersRouter.post('/', async (request, response) => {
   const savedUser = await user.save();
 
   response.status(201).json(savedUser);
+});
+
+//return all users
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({});
+  response.json(users);
 });
 
 module.exports = usersRouter;
---
+++ tests/user_api.test.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+
+const api = supertest(app);
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash('sekret', 10);
+    const user = new User({ username: 'root', passwordHash });
+
+    await user.save();
+  });
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'samuelNze',
+      firstname: 'samuel',
+      lastname: 'nzekwe',
+      location: 'berlin',
+      phone: 123456,
+      email: 'sam@gmail.com',
+      password: 'salainet',
+    };
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map((u) => u.username);
+    expect(usernames).toContain(newUser.username);
+  });
+});
